b28056b86509eb9ad3b523ed34f373d481114f94
Guido Grazioli <guido.grazioli@gmail.com>
2013-04-13 13:07:04 +1000

    support <sumodulename>.routes route declarations filename

--- play-pure-maven-plugin/src/main/scala/com/nominum/build/PlayRoutesCompiler.scala
+++ play-pure-maven-plugin/src/main/scala/com/nominum/build/PlayRoutesCompiler.scala
@@ -26,13 +26,19 @@ class PlayRoutesCompiler {
   def compile(confDirectory: File, generatedDir: File, additionalImports: Seq[String]) = {
 
     (Array(new File(generatedDir, "routes.java")) ++ filesInDirStartingWith(generatedDir, "routes_*")).filter(_ == null).map(GeneratedSource(_)).foreach(_.sync())
-    val routesFile = new File(confDirectory, "routes")
-    try {
-      RoutesCompiler.compile(routesFile, generatedDir, additionalImports)
-    } catch {
-      case e: RoutesCompilationError => throw new MojoExecutionException("Error in routes file on line " + e.line, e)
-    }
-    (filesInDirStartingWith(generatedDir, "routes_*") ++ Array(new File(generatedDir, "routes.java"))).map(_.getAbsoluteFile)
-
+      var routesFile = new File(confDirectory, "routes")
+      if (!routesFile.exists) {
+         for (file <- confDirectory.listFiles.filter(_.getName.endsWith("routes"))) {
+           if (file.exists) routesFile = file
+         }
+      }
+      try {
+        RoutesCompiler.compile(routesFile, generatedDir, additionalImports)
+      } catch {
+        case e: RoutesCompilationError => throw new MojoExecutionException("Error in routes file on line " + e.line, e)
+      }
+      (filesInDirStartingWith(generatedDir, "routes_*") ++ Array(new File(generatedDir, "routes.java"))).map(_.getAbsoluteFile)
   }
+
+  
 }
